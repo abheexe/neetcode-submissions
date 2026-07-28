51dedfd2366de8947176a2effaef4b4c34f6a237
Abhee <145278091+abheexe@users.noreply.github.com>
2026-07-28 12:45:02 +0530

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    vector<int> res;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        inorder(root);
+        return res;
+    }
+private:
+    void inorder(TreeNode* node){
+        if(!node){
+            return;
+        }
+        inorder(node->left);
+        res.push_back(node->val);
+        inorder(node->right);
+    }
+};
